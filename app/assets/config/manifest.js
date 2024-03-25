e1ed6c801b8981b89a670deaf45f7182d237ae1c
kevin stal <kevin.stal@gmx.ch>
2024-03-25 13:27:17 +0100

    fixed javascript finally

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_directory ../javascripts .js
-//= link controllers/pokemon.js
+//= link controllers/application.js
+//= link controllers/index.js
